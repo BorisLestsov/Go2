f86e99009662a611d348b00546a3bab8d938194f
boris <bor1996@mail.ru>
2017-11-14 03:20:57 +0300

    need fix

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "time"
+    "./graph"
+    "os"
+    "strconv"
+)
+
+
+func main() { 
+    rand.Seed(int64(time.Now().Unix()))
+
+    quitCh       := make(chan struct{})
+    NProc, _     := strconv.Atoi(os.Args[1])
+    BasePort, _  := strconv.Atoi(os.Args[2])
+    MinDegree, _ := strconv.Atoi(os.Args[3])
+    MaxDegree, _ := strconv.Atoi(os.Args[4])
+    BaseTtl, _   := strconv.Atoi(os.Args[5])
+
+    gr := graph.Generate(NProc, MinDegree, MaxDegree, BasePort)
+    fmt.Println(gr)
+
+    for i := 0; i < NProc; i++ {
+        go proc(i, gr, quitCh, BaseTtl)
+    }
+
+    for i := 0; i < NProc; i++ {
+	    <-quitCh
+	}
+    time.Sleep(time.Millisecond * 100)
+}
--- proc.go
+++ proc.go
@@ -13,16 +13,19 @@ import (
 
 func ManageConn(Conn *net.UDPConn,
                 dataCh chan msg.Message,
-                timeoutCh chan time.Duration) {
+                timeoutCh chan time.Duration,
+                quitCh chan struct{}) {
     var buffer = make([]byte, 4096)
     var m msg.Message
     //var data string
     var timeout time.Duration
     timeout = time.Duration(0)
 
+    Loop:
     for {
         select {
             case timeout = <-timeoutCh: {}
+            case _ = <-quitCh: {break Loop}
             default: {
                 if timeout != 0 {
                     Conn.SetReadDeadline(time.Now().Add(timeout))
@@ -53,12 +56,17 @@ type Task struct {
     Dst *net.UDPAddr
 }
 
-func ManageSendQueue(MyConn *net.UDPConn, taskCh chan Task, SendInterval time.Duration) {
+func ManageSendQueue(MyConn *net.UDPConn, 
+                     taskCh chan Task, 
+                     SendInterval time.Duration,
+                     quitCh chan struct{}) {
     var t Task
     var buffer = make([]byte, 4096)
     
+    Loop:
     for {
         select {
+            case _ = <-quitCh: {break Loop}
             case t = <- taskCh: {
                 time.Sleep(SendInterval)
                 buffer = t.Msg.ToJsonMsg()
@@ -72,7 +80,10 @@ func ManageSendQueue(MyConn *net.UDPConn, taskCh chan Task, SendInterval time.Du
 
 func proc(MyID int, 
           gr graph.Graph,
-          quitCh chan struct{}) {
+          quitCh chan struct{},
+          BaseTtl int) {
+    
+    rand.Seed(int64(time.Now().Unix()) + int64(MyID))
 
     Neig, _ := gr.Neighbors(MyID)
     NeigAmt := len(Neig)
@@ -102,42 +113,156 @@ func proc(MyID int,
     taskCh    := make(chan Task, 4096)
     timeoutCh := make(chan time.Duration, 1)
 
-    timeoutCh <- time.Duration(time.Second * 2)
+    timeoutCh <- time.Duration(time.Millisecond * 5)
 
+    AllGot := make([]bool, len(gr))
+    TtlMap := make(map[int]int)
+    BaseConfID := 5000
+
+    qCh1 := make(chan struct{}, 1)
+    qCh2 := make(chan struct{}, 1)
+
+    go ManageConn(MyConn, dataCh, timeoutCh, qCh1)
+    go ManageSendQueue(MyConn, taskCh, time.Microsecond * 50, qCh2)
+
+    // init gossip
     if MyID == 0 {
         Dst := rand.Intn(NeigAmt)
         SendAddr := NeigAddrArr[Dst]
         m := Task{msg.Message{ID_: 0, Type_: "msg", Sender_: 0, Origin_: 0, Data_: "kek"}, SendAddr}
+        fmt.Println(MyID, "sending", m, "to", SendAddr)
         taskCh <- m
+
+        for index, _ := range AllGot {
+            AllGot[index] = false
+        }
+
     }
 
-    go ManageConn(MyConn, dataCh, timeoutCh)
-    go ManageSendQueue(MyConn, taskCh, time.Second * 1)
-
     var m msg.Message
+    ticks := 0
+    DEBUG_OUTPUT := true
 
+    MainLoop:
     for {
+        ticks += 1
         select {
             case m = <- dataCh: {
-                if m.Type_ != "timeout" {
+                if m.Type_ != "timeout" && DEBUG_OUTPUT {
                     fmt.Println(MyID, "got msg:", m)
-                
+                }
+                if m.Type_ == "msg" {
                     if !(NeigAmt <= 1) {
-                        Dst := rand.Intn(NeigAmt)
-                        for ; NeigIdArr[Dst] == m.Sender_ ; {
-                            Dst = rand.Intn(NeigAmt)
-                            //fmt.Println(Dst)
+                        _, ok := TtlMap[m.ID_]
+                        if !ok {
+                            TtlMap[m.ID_] = BaseTtl
                         }
-                        SendAddr := NeigAddrArr[Dst]
-                        m.Sender_ = MyID
 
-                        // send
-                        taskCh <- Task{m, SendAddr}
+                        SenderTmp :=  m.Sender_
+                        
+                        if TtlMap[m.ID_] > 0 {
+                            for ; TtlMap[m.ID_] > 0; {
+                                Dst := rand.Intn(NeigAmt)
+                                for ; NeigIdArr[Dst] == SenderTmp ; {
+                                    Dst = rand.Intn(NeigAmt)
+                                    //fmt.Println(Dst)
+                                }
+                                SendAddr := NeigAddrArr[Dst]
+                                m.Sender_ = MyID
+
+                                // pass further
+                                fmt.Println(MyID, "sending", m, "to", SendAddr)
+                                taskCh <- Task{m, SendAddr}
+                                TtlMap[m.ID_] -= 1
+                            }
+
+                            // ID of message with conf
+                            confID := m.ID_+BaseConfID+MyID
+                            _, ok = TtlMap[confID]
+                            if !ok {
+                                TtlMap[confID] = BaseTtl
+                            }
+
+                            // conf msg
+                            m := msg.Message{ID_: confID, Type_: "conf", Sender_: MyID, Origin_: m.Origin_, Data_: strconv.Itoa(MyID)}
+                            for ; TtlMap[confID] > 0; {
+                                Dst := rand.Intn(NeigAmt)
+                                for ; NeigIdArr[Dst] == SenderTmp ; {
+                                    Dst = rand.Intn(NeigAmt)
+                                    //fmt.Println(Dst)
+                                }
+                                SendAddr := NeigAddrArr[Dst]
+                                // send conf
+                                fmt.Println(MyID, "sending", m, "to", SendAddr)
+                                taskCh <- Task{m, SendAddr}
+                                TtlMap[confID] -= 1
+                            }
+                        } else {
+                                fmt.Println(MyID, "discarded", m)
+                        }
                     }
+                } else if m.Type_ == "conf" {
+                    if m.Origin_ == MyID {
+                        // confurmed message
+                        confID, err := strconv.Atoi(m.Data_)
+                        msg.CheckError(err)
+                        AllGot[confID] = true
+                        fmt.Println(AllGot)
+                        all := true
+                        LocalLoop:
+                        for i := range AllGot {
+                            if AllGot[i] == false {
+                                all = false
+                                break LocalLoop
+                            }
+                        }
+                        if all {
+                            fmt.Println("Finished in", ticks, "ticks")
+                            qCh1 <- struct{}{}
+                            qCh2 <- struct{}{}
+                            break MainLoop
+                        }
+                    } else {
+                        //pass further
+                        if !(NeigAmt <= 1) {
+                            _, ok := TtlMap[m.ID_]
+                            if !ok {
+                                TtlMap[m.ID_] = BaseTtl
+                            }
+
+                            SenderTmp :=  m.Sender_
+                            
+                            if TtlMap[m.ID_] > 0 {
+                                for ; TtlMap[m.ID_] > 0; {
+                                    Dst := rand.Intn(NeigAmt)
+                                    for ; NeigIdArr[Dst] == SenderTmp ; {
+                                        Dst = rand.Intn(NeigAmt)
+                                        //fmt.Println(Dst)
+                                    }
+                                    SendAddr := NeigAddrArr[Dst]
+                                    m.Sender_ = MyID
+
+                                    // pass further
+                                    fmt.Println(MyID, "sending", m, "to", SendAddr)
+                                    taskCh <- Task{m, SendAddr}
+                                    TtlMap[m.ID_] -= 1
+                                }
+                            } else {
+                                fmt.Println(MyID, "discarded", m)
+                            }
+                        }
+                    }
+                } else if m.Type_ == "timeout" {
+                    qCh1 <- struct{}{}
+                    qCh2 <- struct{}{}
+                    break MainLoop
+                } else {
+                    panic("Unknown message type"+m.Type_)
                 }
             }
         }
     }
 
+    //fmt.Println(MyID, "exited")
     quitCh <- struct{}{}
 }
